f0efd5752138a31447dec4bf62c7d8565ef420fe
wxyzlines <stoutshako42ref.zglj9@slmail.me>
2022-09-07 14:28:28 +0800

    added base game function in game.js

--- game.js
+++ game.js
@@ -1,15 +1,60 @@
-choices = ['Rock', 'Paper', 'Scissors']
+choices = ['rock', 'paper', 'scissors']
+let playerScore = 0
+let computerScore = 0
 
 function getComputerChoice() {  
     // math random is inclusive 1
-    return choices[parseInt(Math.random() * 3)] 
-
+    return choices[Math.floor((Math.random() * 3))] 
 }
 function playRound(playerSelection, computerSelection) {
-    return
+    if (playerSelection == "rock" && computerSelection == "scissors" ||
+    playerSelection == "paper" && computerSelection == "rock" || 
+    playerSelection == "scissors" && computerSelection == "paper") {
+        return "win"
+    }
 
+    else if (playerSelection == computerSelection) {
+        return "tie"
+    }
 
-    
+    else {
+        return "lose"
+    }
+           
 }
 
-console.log(getComputerChoice())
+function game() {
+    let result = ""
+    let playerChoice = ""
+    let computerChoice = ""
+    console.log(computerChoice)
+
+    for (i = 0; i < 5; i++) {
+        playerChoice = prompt("write your choice")
+        computerChoice = getComputerChoice()
+        result = playRound(playerChoice,computerChoice)
+
+        console.log(playerChoice + " vs " + computerChoice)
+        if (result == 'win') {
+            playerScore++
+        }
+        else if (result == 'lose') {
+            computerScore++
+        }
+        else if (result == 'tie') {
+            i--
+        }
+        
+        if (playerScore == 3) {
+            console.log("player wins!")
+            break;
+        }
+
+        else if (computerScore == 3) {
+            console.log("player lost...")
+            break;
+        }
+    }
+}
+
+game()
