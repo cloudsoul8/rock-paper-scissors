b1ae090b2766346864b454a6ef71a1d4bf14f457
wxyzlines <stoutshako42ref.zglj9@slmail.me>
2022-09-07 10:57:39 +0800

    added index.html and game.js

---
+++ game.js
@@ -0,0 +1,13 @@
+choices = ['Rock', 'Paper', 'Scissors']
+
+function getComputerChoice() {  
+    // math random is inclusive 1
+    return choices[parseInt(Math.random() * 3)] 
+
+}
+function playRound(playerSelection, computerSelection) {
+    //if ()
+    return 
+}
+
+console.log(getComputerChoice())
